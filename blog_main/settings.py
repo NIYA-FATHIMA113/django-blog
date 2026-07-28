7bda6587b3b48275731d8642729d39aab2d3ede1
Niya <niyafathima113@gmail.com>
2026-07-28 18:46:14 +0530

    fix static files using whitenoise

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,7 +135,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'blog_main' / 'static',
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL='/media/'
 MEDIA_ROOT=BASE_DIR/'media'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
