c8eab9a5e5e3ec177206234cd51adabcc81c4163
Niya <niyafathima113@gmail.com>
2026-07-31 15:20:55 +0530

    fix django 6 cloudinary storage

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -164,3 +164,9 @@ cloudinary.config(
     secure=True,
 )
 
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.environ.get("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.environ.get("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.environ.get("CLOUDINARY_API_SECRET"),
+}
+
