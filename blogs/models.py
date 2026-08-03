52299485878d18f73abd328def93fa464d46424e
Niya <niyafathima113@gmail.com>
2026-08-03 15:59:30 +0530

    automatic slug

--- blogs/models.py
+++ blogs/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import slugify
+
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
@@ -11,7 +13,7 @@ class Category(models.Model):
         return self.category_name
 
     class Meta:
-        verbose_name_plural = 'categorities'
+        verbose_name_plural = 'categories'
 
 
 STATUS_CHOICES = (
@@ -51,16 +53,24 @@ class Blog(models.Model):
 
     updated_at = models.DateTimeField(auto_now=True)
 
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super().save(*args, **kwargs)
+
+
     def __str__(self):
         return self.title
 
 
 class Comment(models.Model):
-    user=models.ForeignKey(User,on_delete=models.CASCADE)
-    blog=models.ForeignKey(Blog,on_delete=models.CASCADE)
-    comment=models.TextField(max_length=250)
-    created_at=models.DateTimeField(auto_now_add=True)
-    updated_at=models.DateTimeField(auto_now=True)
-    def __str__(self):
-            return self.comment
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    comment = models.TextField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.comment
