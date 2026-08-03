9bfcebee0495d535b0664be90746389f7981c7fe
Niya <niyafathima113@gmail.com>
2026-08-03 13:37:30 +0530

    render deployment issue

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -30,12 +30,12 @@ SECRET_KEY = os.environ.get(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = os.environ.get("DEBUG", "False" if os.environ.get("RENDER") else "True") == "True"
 
-ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS",
-    "127.0.0.1,localhost"
-).split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+render_hostname = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if render_hostname:
+    ALLOWED_HOSTS.append(render_hostname)
 
 
 # Application definition
@@ -97,7 +97,6 @@ WSGI_APPLICATION = 'blog_main.wsgi.application'
 
 database_url = os.environ.get("DATABASE_URL")
 IS_LOCAL_DEVELOPMENT = not bool(database_url)
-print("DATABASE_URL =", repr(database_url))
 if database_url:
     DATABASES = {
         "default": dj_database_url.parse(database_url)
